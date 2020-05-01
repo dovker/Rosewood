997b99cf24edb590035244dc3c91fe5d7c1f5e70
iHelp <dovker@gmail.com>
2020-05-01 16:10:21 +0300

    Added ImGui Docking

--- Rosewood/src/Rosewood/ImGui/ImGuiLayer.h
+++ Rosewood/src/Rosewood/ImGui/ImGuiLayer.h
@@ -14,26 +14,17 @@ namespace Rosewood {
 		ImGuiLayer();
 		~ImGuiLayer();
 
-		void OnAttach();
-		
-		void OnDetach();
+		virtual void OnAttach() override;
+		virtual void OnDetach() override;
+		virtual void OnImGuiRender() override;
 
-		void OnUpdate();
-		void OnEvent(Event& event);
+		void Begin();
+		void End();
 
 
 	private:
 		float m_Time = 0.0f;
 
-		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
-		bool OnMouseMovedEvent(MouseMovedEvent& e);
-		bool OnKeyPressedEvent(KeyPressedEvent& e);
-		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
-		bool OnKeyTypedEvent(KeyTypedEvent& e);
-		bool OnWindowResizeEvent(WindowResizeEvent& e);
-		
 	};
 
 }
